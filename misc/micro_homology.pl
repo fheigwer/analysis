2456696b3cb0a676fee3a317d948c7e5d4c348da
fheigwer <florian_heigwer@web.de>
2014-05-07 15:50:40 +0200

    finished micro homology scripts

--- misc/micro_homology.pl
+++ misc/micro_homology.pl
@@ -5,30 +5,24 @@ use warnings;
 #my $string="ATGCTAGCAGCTGATGCTGCTGCTAGCTGCTGATGCTAGTGGGGATGCGTAAGATGCTAGATGCTAGTGGTAGTGCTAGCTAGCTGCATGATCGTACTACGACTG";
 
 #my $string="ATGCTAATGGATCGAATGCTAATGGATCGATCGTAGCTATGCGACGATGCTAATGGATCGAATGCTAATGGATCGATCGTAGCTATGCGACGACATCGACGATCGTAGCTATGCGACGACATCGACGAATGCTAATGGATCGAATGCTAATGGATCGATCGTAGCTATGCGACGACATCGACGATCGTAGCTATGCGACGACATCGACGAACATCGACGATCGTAGCTATGCGACGACATCGACGA";
-my $string="";
-my %hash=('0'=>"A",'1'=>"G",'2'=>"C",'3'=>"T");
-foreach my $number (1..1000){
-	$string.=$hash{int(rand(3))};
-}
-
-print "string done\n";
-
-
-
-my $position=300;
-my $threshold=20;
-my $length=5;
-
+	
+foreach my $stu (1..100000){
+	my $string="";
+	my %hash=('0'=>"A",'1'=>"G",'2'=>"C",'3'=>"T");
+	foreach my $number (1..1000){
+		$string.=$hash{int(rand(3))};
+	}
+	#print "string done\n";
+	my $position=300;
+	my $threshold=20;
+	my $length=5;
 	my %postitions;
 	%{$postitions{"A"}}=make_pos_index(\$string,"A");
 	%{$postitions{"G"}}=make_pos_index(\$string,"G");
 	%{$postitions{"C"}}=make_pos_index(\$string,"C");
 	%{$postitions{"T"}}=make_pos_index(\$string,"T");
-	
-	
-#foreach my $stu (1..10000){
-score_micro_homolgy(\%postitions,$threshold,$position,$length);
-#}
+	print score_micro_homolgy(\%postitions,$threshold,$position,$length,\$string)."\n";
+}
 
 
 
@@ -44,18 +38,20 @@ sub score_micro_homolgy {
 	#my $length=$_[3];
 	#my $postitions=%{$_[0]};
 	my $stuff=0;
+	my $outframe=1;
+	my $inframe=1;
 	RIGTHSTART:foreach my $rightarmstart ($_[2]..($_[2]+$_[1])){		
 		LENGTH:foreach my $length (2..$_[3]){
 			if($lengthhom>0){
 				$lengthhom=0;
-				my @right_seq=split("",substr($string,$rightarmstart,$length));
+				my @right_seq=split("",substr(${$_[4]},$rightarmstart,$length));
 				$stuff=$length+1;
 				LEFTARM: while($stuff<$_[1]){
 					$stuff++;
 					$count=0;
 					$seq="";
 					foreach my $letter (@right_seq){
-						if(exists(${${$_[0]}{$letter}}{($position-$stuff+$count)})){
+						if(exists(${${$_[0]}{$letter}}{($_[2]-$stuff+$count)})){
 							$seq.=$letter;
 							$count++;												
 						}else{
@@ -64,11 +60,17 @@ sub score_micro_homolgy {
 					}
 					if($count==scalar(@right_seq)){
 						$lengthhom=1;
-						print "(@right_seq): $seq ".scalar(@right_seq)." ".$count." $stuff $position $rightarmstart hit"."\n";
-						print substr($string,($position-20),40)."\n";
-						print substr($string,($position-20),(20-$stuff+$count));
-						print '-' x ($stuff+($rightarmstart-$position-$count)); 
-						print substr($string,($rightarmstart),20-($rightarmstart-$position))."\n";
+						my $gaplength=($stuff+($rightarmstart-$_[2]));
+						if($gaplength%3 == 0){
+							$inframe=$inframe+($count*exp(0.1*(-$gaplength)));
+						}else{
+							$outframe=$outframe+($count*exp(0.1*(-$gaplength)));
+						}
+						#print "(@right_seq): $seq ".scalar(@right_seq)." ".$count." $stuff $position $rightarmstart hit"."\n";
+						#print substr($string,($position-20),40)."\n";
+						#print substr($string,($position-20),(20-$stuff+$count));
+						#print '-' x ($stuff+($rightarmstart-$position-$count)); 
+						#print substr($string,($rightarmstart),20-($rightarmstart-$position))."\n";
 					}else{
 						next LEFTARM;
 					}
@@ -79,7 +81,7 @@ sub score_micro_homolgy {
 			}	
 		}
 	}
-
+	return log($outframe/$inframe);
 }
 
 sub make_pos_index {
