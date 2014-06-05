ce64d885a7a1138150e91b2135bf247ed017c3f6
Heigwer <f.heigwer@dkfz.de>
2014-06-05 13:06:00 +0200

    edited comments

--- crispr_off_target/final_filter_vcf.pl
+++ crispr_off_target/final_filter_vcf.pl
@@ -5,9 +5,9 @@ while(<$infile>){
 	if($_=~m/^X\s/){
 		my @line=split("\t",$_);
 		if($line[5]>40){#filter for QUAL greater 40
-			if($line[7]=~m/DP=(\d+)/){	#filter for depth greater 4
+			if($line[7]=~m/DP=(\d+)/){	#filter for depth greater 40
 				if($1>40){
-					if($line[7]=~m/IDV=(\d+)/){
+					if($line[7]=~m/IDV=(\d+)/){ #filter for number of supportive reads
 						if($1>5){
 							if($line[9]=~m/([\d\.]+)[\/\|]([\d\.]+)/){  #filter to get heterozygous mutations only
 								$S11=$1;$S12=$2;
